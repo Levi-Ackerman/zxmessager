2b007596aa37925d21d53ae520b671176b5abe14
zhengxian.lzx <lizhengxian1991@126.com>
2017-11-17 17:36:16 +0800

    import ButterKnife

--- app/src/main/java/top/lizhengxian/signal/MainActivity.java
+++ app/src/main/java/top/lizhengxian/signal/MainActivity.java
@@ -2,14 +2,25 @@ package top.lizhengxian.signal;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import top.lizhengxian.event_lib.ZxMessager;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btn) protected Button mBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn)
+    protected void onClick(View view){
         ZxMessager.post(1,"Hello world!");
     }
 }
